6302f04988751852c78c3e9335805025672786e7
Gregory Amerson <gregory.amerson@liferay.com>
2025-09-18 12:00:55 -0500

    add recipe for lpd65526

---
+++ recipes/lpd65526-better-batch-errors/workspace/modules/block-oauth-requests/src/main/java/com/liferay/block/oauth/requests/BlockRequestFilter.java
@@ -0,0 +1,56 @@
+package com.liferay.block.oauth.request;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.servlet.BaseFilter;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Gregory Amerson
+ */
+@Component(
+        immediate = true,
+        property = {
+                "before-filter=Auto Login Filter",
+                "dispatcher=REQUEST",
+                "servlet-context-name=",
+                "servlet-filter-name=Block Servlet Filter",
+                "url-pattern=/o/oauth2/*"
+        },
+        service = Filter.class
+)
+public class BlockRequestFilter extends BaseFilter {
+
+    @Activate
+    public void activate() {
+        _log.info("Activating BlockRequestFilter");
+    }
+
+    @Override
+    protected Log getLog() {
+        return _log;
+    }
+
+    @Override
+    protected void processFilter(
+            HttpServletRequest httpServletRequest,
+            HttpServletResponse httpServletResponse, FilterChain filterChain)
+            throws Exception {
+
+        httpServletResponse.sendError(HttpServletResponse.SC_FORBIDDEN, "xXx Access to this resource is forbidden");
+
+        processFilter(
+                BlockRequestFilter.class.getName(), httpServletRequest,
+                httpServletResponse, filterChain);
+    }
+
+    private static final Log _log = LogFactoryUtil.getLog(
+            BlockRequestFilter.class);
+
+}
