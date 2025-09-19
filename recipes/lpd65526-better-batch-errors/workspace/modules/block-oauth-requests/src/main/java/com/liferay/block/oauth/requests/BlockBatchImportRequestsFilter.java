57dc43bb44e7484f4f40d88a0678b30c8e2b4c62
Gregory Amerson <gregory.amerson@liferay.com>
2025-09-19 10:30:03 -0500

    split into three requests

--- recipes/lpd65526-better-batch-errors/workspace/modules/block-oauth-requests/src/main/java/com/liferay/block/oauth/requests/BlockBatchImportRequestsFilter.java
+++ recipes/lpd65526-better-batch-errors/workspace/modules/block-oauth-requests/src/main/java/com/liferay/block/oauth/requests/BlockBatchImportRequestsFilter.java
@@ -3,7 +3,6 @@ package com.liferay.block.oauth.requests;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.servlet.BaseFilter;
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 
 import javax.servlet.Filter;
@@ -20,17 +19,12 @@ import javax.servlet.http.HttpServletResponse;
                 "before-filter=Auto Login Filter",
                 "dispatcher=REQUEST",
                 "servlet-context-name=",
-                "servlet-filter-name=Block Servlet Filter",
-                "url-pattern=/o/oauth2/*"
+                "servlet-filter-name=Block Batch Import Requests Servlet Filter",
+                "url-pattern=/o/headless-batch-engine/*"
         },
         service = Filter.class
 )
-public class BlockRequestsFilter extends BaseFilter {
-
-    @Activate
-    public void activate() {
-        _log.info("Activating BlockRequestFilter");
-    }
+public class BlockBatchImportRequestsFilter extends BaseFilter {
 
     @Override
     protected Log getLog() {
@@ -43,16 +37,16 @@ public class BlockRequestsFilter extends BaseFilter {
             HttpServletResponse httpServletResponse, FilterChain filterChain)
             throws Exception {
 
-        //httpServletResponse.sendError(HttpServletResponse.SC_FORBIDDEN, "xXx Access to this resource is forbidden");
+        httpServletResponse.sendError(HttpServletResponse.SC_FORBIDDEN, "xXx Access to this resource is forbidden");
         //httpServletResponse.sendError(HttpServletResponse.SC_BAD_REQUEST, "Bad request; I'm grumpy");
-        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized access to resource");
+        //httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized access to resource");
 
         processFilter(
-                BlockRequestsFilter.class.getName(), httpServletRequest,
+                BlockBatchImportRequestsFilter.class.getName(), httpServletRequest,
                 httpServletResponse, filterChain);
     }
 
     private static final Log _log = LogFactoryUtil.getLog(
-            BlockRequestsFilter.class);
+            BlockBatchImportRequestsFilter.class);
 
 }
---
+++ recipes/lpd65526-better-batch-errors/workspace/modules/block-oauth-requests/src/main/java/com/liferay/block/oauth/requests/BlockTokenRequestsFilter.java
@@ -0,0 +1,52 @@
+package com.liferay.block.oauth.requests;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.servlet.BaseFilter;
+import org.osgi.service.component.annotations.Component;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Gregory Amerson
+ */
+@Component(
+        immediate = true,
+        property = {
+                "before-filter=Auto Login Filter",
+                "dispatcher=REQUEST",
+                "servlet-context-name=",
+                "servlet-filter-name=Block Token Requests Servlet Filter",
+                "url-pattern=/o/oauth2/token"
+        },
+        service = Filter.class
+)
+public class BlockTokenRequestsFilter extends BaseFilter {
+
+    @Override
+    protected Log getLog() {
+        return _log;
+    }
+
+    @Override
+    protected void processFilter(
+            HttpServletRequest httpServletRequest,
+            HttpServletResponse httpServletResponse, FilterChain filterChain)
+            throws Exception {
+
+        //httpServletResponse.sendError(HttpServletResponse.SC_FORBIDDEN, "xXx Access to this resource is forbidden");
+        //httpServletResponse.sendError(HttpServletResponse.SC_BAD_REQUEST, "Bad request; I'm grumpy");
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized access to resource");
+
+        processFilter(
+                BlockTokenRequestsFilter.class.getName(), httpServletRequest,
+                httpServletResponse, filterChain);
+    }
+
+    private static final Log _log = LogFactoryUtil.getLog(
+            BlockTokenRequestsFilter.class);
+
+}
